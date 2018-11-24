e737660861d6cac99da94b85986e1bc325d4dffd
mcvillarreal <m_villarreal@javeriana.edu.co>
2018-11-24 16:12:52 -0500

    My Django Girls app, fourth commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+#método para llamar los posts dentro de la vista
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
